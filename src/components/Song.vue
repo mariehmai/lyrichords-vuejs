c078deca78312c4340bbe9750abb6f5b6766ea92
mhmai <mhm@wemanity.com>
2019-10-06 01:15:35 +0200

    feat(Song): add basic component to display a song lyrics

---
+++ src/components/Song.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="song-container">
+    <h2>{{ author }} - {{ title }}</h2>
+    <span v-for="(next, i) in lyrics" :key="i">
+      {{ next.sentence }}
+      <br>
+      <br v-if="next.isEndVerse">
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    lyrics: { type: Array, required: true }
+  }
+};
+</script>
+
+<style scoped>
+h2 {
+  padding-bottom: 2vh;
+}
+</style>
