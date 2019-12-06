062434acea0185cb37227a41fa31db1904ca338b
mhmai <mhm@wemanity.com>
2019-12-06 17:30:17 +0100

    feat(CardViewer): refactor card removing useless template element

--- src/components/CardViewer.vue
+++ src/components/CardViewer.vue
@@ -1,16 +1,12 @@
 <template>
-  <v-col md="4" offset-md="8">
-    <v-card :elevation="4">
-      <v-card-text>
-        <Song
-          v-if="type === 'song'"
-          :title="children.title"
-          :author="children.author"
-          :lyrics="children.lyrics"
-        />
-      </v-card-text>
-    </v-card>
-  </v-col>
+  <v-card :elevation="4">
+    <Song
+      v-if="type === 'song'"
+      :title="children.title"
+      :author="children.author"
+      :lyrics="children.lyrics"
+    />
+  </v-card>
 </template>
 
 <script>
@@ -26,9 +22,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-.song {
-  padding: 4vh 3vh;
-}
-</style>
