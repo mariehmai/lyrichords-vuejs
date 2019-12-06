cdb3f144ae3774b13d97769dd61a2ef9bcaf0a45
mhmai <mhm@wemanity.com>
2019-12-06 17:23:55 +0100

    ref(TrackCard): update component to have list of tracks and random colors assigned

--- src/components/TrackCard.vue
+++ src/components/TrackCard.vue
@@ -1,17 +1,26 @@
 <template>
-  <v-card class="track-card" :elevation="4">
-    <v-img class="white--text align-end" width="300px" height="220px" :src="cover">
-      <v-card-title>{{title}}</v-card-title>
-    </v-img>
-    <v-card-text>
-      <div>{{ $t('artist') }}: {{artist}}</div>
-      <div>{{ $t('genre') }}: {{genre}}</div>
-    </v-card-text>
+  <v-card class="track-card" :color="randomColor()" dark>
+    <div class="d-flex justify-space-between">
+      <div>
+        <v-card-title v-text="title"></v-card-title>
+        <v-card-text v-text="artist"></v-card-text>
+      </div>
+      <v-avatar class="ma-3" size="70" tile>
+        <v-img :src="cover"></v-img>
+      </v-avatar>
+    </div>
   </v-card>
 </template>
 
 <script>
 const unknown = require('@/assets/unknown.svg');
+const colors = [
+  'pink darken-2',
+  'grey darken-2',
+  'green darken-2',
+  'yellow darken-2',
+  'blue darken-3'
+];
 
 export default {
   props: {
@@ -19,6 +28,11 @@ export default {
     title: { type: String, required: true, default: 'Unknown' },
     genre: { type: String, required: true, default: 'Unknown' },
     cover: { type: String, default: unknown }
+  },
+  methods: {
+    randomColor() {
+      return colors[Math.floor(Math.random() * colors.length)];
+    }
   }
 };
 </script>
