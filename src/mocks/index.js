0386689f4cae31e29987085121059373b1bf702d
mhmai <mhm@wemanity.com>
2019-12-06 16:07:28 +0100

    feat(mocks): add mock data for tracks and song

---
+++ src/mocks/index.js
@@ -0,0 +1,128 @@
+export default {
+  song: {
+    title: 'Eu Sei',
+    author: 'Papas da Lingua',
+    lyrics: [
+      { isEndVerse: false, sentence: 'Eu sei, tudo pode acontecer' },
+      { isEndVerse: false, sentence: 'Eu sei, nosso amor não vai morrer' },
+      { isEndVerse: false, sentence: 'Vou pedir aos céus, você aqui comigo' },
+      {
+        isEndVerse: true,
+        sentence: 'Vou jogar, no mar, flores pra te encontrar'
+      },
+      { isEndVerse: false, sentence: 'Não sei por que você disse adeus' },
+      { isEndVerse: false, sentence: 'Guardei o beijo que você me deu' },
+      { isEndVerse: false, sentence: 'Vou pedir aos céus, você aqui comigo' },
+      {
+        isEndVerse: true,
+        sentence: 'Vou jogar, no mar, flores pra te encontrar'
+      },
+      { isEndVerse: false, sentence: 'You say goodbye, and I say hello' },
+      { isEndVerse: true, sentence: 'You say goodbye, and I say hello' },
+      { isEndVerse: true, sentence: 'Iê, iê, iê, iê, iê, iê' },
+      { isEndVerse: false, sentence: 'Não sei por que você disse adeus' },
+      { isEndVerse: false, sentence: 'Guardei o beijo que você me deu' },
+      { isEndVerse: false, sentence: 'Vou pedir aos céus, você aqui comigo' },
+      {
+        isEndVerse: false,
+        sentence: 'Vou jogar, no mar, flores pra te encontrar'
+      },
+      { isEndVerse: true, sentence: 'Eu sei, tudo pode acontecer' },
+      { isEndVerse: false, sentence: 'You say goodbye, and I say hello' },
+      { isEndVerse: true, sentence: 'You say goodbye, and I say hello' },
+      { isEndVerse: false, sentence: 'Iê, iê, iê, iê, iê, iê' }
+    ]
+  },
+  tracks: [
+    {
+      id: 1,
+      title: 'Eu Sei',
+      artist: 'Papas da Lingua',
+      cover: 'https://i.ytimg.com/vi/6G7sj2V1QYM/maxresdefault.jpg',
+      genre: 'Pop'
+    },
+    {
+      id: 2,
+      title: 'You are the reason',
+      artist: 'Calumn Scott',
+      cover: 'https://www.lacoccinelle.net/1188537-1.jpg?20191117',
+      genre: 'Pop'
+    },
+    {
+      id: 3,
+      title: 'You are the reason',
+      artist: 'Calumn Scott',
+      genre: 'Pop'
+    },
+    {
+      id: 4,
+      title: 'You are the reason',
+      artist: 'Calumn Scott',
+      cover: 'https://www.lacoccinelle.net/1188537-1.jpg?20191117'
+    },
+    {
+      id: 5,
+      title: 'You are the reason',
+      artist: 'Calumn Scott',
+      genre: 'Pop'
+    },
+    {
+      id: 6,
+      title: 'You are the reason',
+      artist: 'Calumn Scott',
+      cover: 'https://www.lacoccinelle.net/1188537-1.jpg?20191117'
+    },
+    {
+      id: 7,
+      title: 'You are the reason',
+      artist: 'Calumn Scott',
+      cover: 'https://www.lacoccinelle.net/1188537-1.jpg?20191117',
+      genre: 'Pop'
+    },
+    {
+      id: 8,
+      title: 'Eu Sei',
+      artist: 'Papas da Lingua',
+      cover: 'https://i.ytimg.com/vi/6G7sj2V1QYM/maxresdefault.jpg',
+      genre: 'Pop'
+    },
+    {
+      id: 9,
+      title: 'You are the reason',
+      artist: 'Calumn Scott',
+      cover: 'https://www.lacoccinelle.net/1188537-1.jpg?20191117',
+      genre: 'Pop'
+    },
+    {
+      id: 10,
+      title: 'You are the reason',
+      artist: 'Calumn Scott',
+      genre: 'Pop'
+    },
+    {
+      id: 11,
+      title: 'You are the reason',
+      artist: 'Calumn Scott',
+      cover: 'https://www.lacoccinelle.net/1188537-1.jpg?20191117'
+    },
+    {
+      id: 12,
+      title: 'You are the reason',
+      artist: 'Calumn Scott',
+      genre: 'Pop'
+    },
+    {
+      id: 13,
+      title: 'You are the reason',
+      artist: 'Calumn Scott',
+      cover: 'https://www.lacoccinelle.net/1188537-1.jpg?20191117'
+    },
+    {
+      id: 14,
+      title: 'You are the reason',
+      artist: 'Calumn Scott',
+      cover: 'https://www.lacoccinelle.net/1188537-1.jpg?20191117',
+      genre: 'Pop'
+    }
+  ]
+};
