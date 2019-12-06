b35b7ea9906b506c0b8a2e0cc9753618960c2449
mhmai <mhm@wemanity.com>
2019-12-06 17:32:44 +0100

    feat(Home): extract song to mocks + display list of tracks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,67 @@
 <template>
-  <CardViewer type="song" :children="song" />
+  <div id="home">
+    <div class="track-container">
+      <TrackCard
+        v-for="track in tracks"
+        :key="track.id"
+        class="track-card"
+        :artist="track.artist"
+        :title="track.title"
+        :cover="track.cover"
+        :genre="track.genre"
+      />
+    </div>
+
+    <div class="song-container">
+      <CardViewer class="card-viewer" type="song" :children="song" />
+    </div>
+  </div>
 </template>
 
 <script>
 import CardViewer from '@/components/CardViewer';
+import TrackCard from '@/components/TrackCard';
 
-const song = {
-  title: 'Eu Sei',
-  author: 'Papas da Lingua',
-  lyrics: [
-    { isEndVerse: false, sentence: 'Eu sei, tudo pode acontecer' },
-    { isEndVerse: false, sentence: 'Eu sei, nosso amor não vai morrer' },
-    { isEndVerse: false, sentence: 'Vou pedir aos céus, você aqui comigo' },
-    {
-      isEndVerse: true,
-      sentence: 'Vou jogar, no mar, flores pra te encontrar'
-    },
-    { isEndVerse: false, sentence: 'Não sei por que você disse adeus' },
-    { isEndVerse: false, sentence: 'Guardei o beijo que você me deu' },
-    { isEndVerse: false, sentence: 'Vou pedir aos céus, você aqui comigo' },
-    {
-      isEndVerse: true,
-      sentence: 'Vou jogar, no mar, flores pra te encontrar'
-    },
-    { isEndVerse: false, sentence: 'You say goodbye, and I say hello' },
-    { isEndVerse: true, sentence: 'You say goodbye, and I say hello' },
-    { isEndVerse: true, sentence: 'Iê, iê, iê, iê, iê, iê' },
-    { isEndVerse: false, sentence: 'Não sei por que você disse adeus' },
-    { isEndVerse: false, sentence: 'Guardei o beijo que você me deu' },
-    { isEndVerse: false, sentence: 'Vou pedir aos céus, você aqui comigo' },
-    {
-      isEndVerse: false,
-      sentence: 'Vou jogar, no mar, flores pra te encontrar'
-    },
-    { isEndVerse: true, sentence: 'Eu sei, tudo pode acontecer' },
-    { isEndVerse: false, sentence: 'You say goodbye, and I say hello' },
-    { isEndVerse: true, sentence: 'You say goodbye, and I say hello' },
-    { isEndVerse: false, sentence: 'Iê, iê, iê, iê, iê, iê' }
-  ]
-};
+import mocks from '@/mocks';
 
 export default {
   components: {
-    CardViewer
+    CardViewer,
+    TrackCard
   },
   data: function() {
-    return { song };
+    return { song: mocks.song, tracks: mocks.tracks };
   }
 };
 </script>
+
+<style scoped>
+#home {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.track-container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  flex: 1;
+  justify-content: center;
+  padding: 2vh 0.5vh;
+}
+
+.track-card {
+  margin: 0.5vh 1vh;
+}
+
+.song-container {
+  flex: 2;
+  padding: 1.5vh;
+}
+
+.card-viewer {
+  margin: 1vh 0.5vh;
+}
+</style>
+
