aa0e1c9c0f3d2583f14214283615d2d584c94598
mhmai <mhm@wemanity.com>
2019-12-06 15:54:43 +0100

    feat(TrackCard): add card component to display track info

---
+++ src/components/TrackCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card class="track-card" :elevation="4">
+    <v-img class="white--text align-end" width="300px" height="220px" :src="cover">
+      <v-card-title>{{title}}</v-card-title>
+    </v-img>
+    <v-card-text>
+      <div>{{ $t('artist') }}: {{artist}}</div>
+      <div>{{ $t('genre') }}: {{genre}}</div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+const unknown = require('@/assets/unknown.svg');
+
+export default {
+  props: {
+    artist: { type: String, required: true, default: 'Unknown' },
+    title: { type: String, required: true, default: 'Unknown' },
+    genre: { type: String, required: true, default: 'Unknown' },
+    cover: { type: String, default: unknown }
+  }
+};
+</script>
