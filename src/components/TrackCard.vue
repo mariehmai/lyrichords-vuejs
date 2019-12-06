6db0bfe9cf1974fc29bbb7855c6ca3218635091c
mhmai <mhm@wemanity.com>
2019-12-06 18:14:04 +0100

    feat(TrackCard): change background color with random img

--- src/components/TrackCard.vue
+++ src/components/TrackCard.vue
@@ -1,26 +1,32 @@
 <template>
-  <v-card class="track-card" :color="randomColor()" dark>
-    <div class="d-flex justify-space-between">
-      <div>
-        <v-card-title v-text="title" />
-        <v-card-text v-text="artist" />
+  <v-card class="track-card" dark>
+    <v-img
+      :src="randomBackground()"
+      height="100px"
+      aspect-ratio="1"
+      gradient="to right, rgba(0,0,0,.1), rgba(0,0,0,1)"
+    >
+      <div class="d-flex justify-space-between">
+        <div>
+          <v-card-title v-text="title" />
+          <v-card-text v-text="artist" />
+        </div>
+        <v-avatar class="ma-3" size="70" tile>
+          <v-img :src="cover" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" />
+        </v-avatar>
       </div>
-      <v-avatar class="ma-3" size="70" tile>
-        <v-img :src="cover" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" />
-      </v-avatar>
-    </div>
+    </v-img>
   </v-card>
 </template>
 
 <script>
 const unknown = require('@/assets/unknown.svg');
-const colors = [
-  'pink darken-2',
-  'grey darken-2',
-  'green darken-2',
-  'yellow darken-2',
-  'blue darken-3'
-];
+const bg1 = require('@/assets/track-bg1.png');
+const bg2 = require('@/assets/track-bg2.png');
+const bg3 = require('@/assets/track-bg3.png');
+const bg4 = require('@/assets/track-bg4.png');
+const bg5 = require('@/assets/track-bg5.png');
+const backgrounds = [bg1, bg2, bg3, bg4, bg5];
 
 export default {
   props: {
@@ -30,8 +36,8 @@ export default {
     cover: { type: String, default: unknown }
   },
   methods: {
-    randomColor() {
-      return colors[Math.floor(Math.random() * colors.length)];
+    randomBackground() {
+      return backgrounds[Math.floor(Math.random() * backgrounds.length)];
     }
   }
 };
