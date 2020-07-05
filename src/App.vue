26c91b0f54b14df3845d9801472424152ee498a9
mariehmai <mai.mariehelene@gmail.com>
2020-07-05 21:15:24 +0200

    refactor(Navbar): extracted Navbar from App

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <div class="text-uppercase headline logo">
-        <v-img class="image" :src="require('@/assets/logo.svg')" />
-        <span class="font-weight-light">{{ appName }}</span>
-      </div>
-      <v-btn icon @click="logout">
-        <font-awesome-icon :icon="['fa', 'sign-out-alt']" />
-      </v-btn>
-    </v-app-bar>
+    <Navbar />
     <v-main>
       <router-view />
     </v-main>
@@ -16,29 +8,12 @@
 </template>
 
 <script>
-import { Action } from '@/store/auth.store';
+import Navbar from '@/components/Navbar';
 
 export default {
   name: 'App',
-  data: () => ({
-    appName: process.env.VUE_APP_TITLE
-  }),
-  methods: {
-    logout() {
-      this.$store.dispatch(Action.LOGOUT);
-    }
+  components: {
+    Navbar
   }
 };
 </script>
-
-<style scoped>
-.logo {
-  display: flex;
-  align-items: center;
-}
-
-.image {
-  width: 44px;
-  margin-right: 8px;
-}
-</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app-bar app>
+    <div class="text-uppercase headline logo">
+      <v-img class="image" :src="require('@/assets/logo.svg')" />
+      <span class="font-weight-light">{{ appName }}</span>
+    </div>
+    <v-btn icon @click="logout">
+      <font-awesome-icon :icon="['fa', 'sign-out-alt']" />
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+import { Action } from '@/store/auth.store';
+
+export default {
+  data: () => ({
+    appName: process.env.VUE_APP_TITLE
+  }),
+  methods: {
+    logout() {
+      this.$store.dispatch(Action.LOGOUT);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.logo {
+  display: flex;
+  align-items: center;
+}
+
+.image {
+  width: 44px;
+  margin-right: 8px;
+}
+</style>
