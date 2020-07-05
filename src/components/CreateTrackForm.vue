0862f8f08683e5eae14ae7a8e12a240850a89c35
mariehmai <mai.mariehelene@gmail.com>
2020-07-05 20:54:27 +0200

    feat(CreateTrackForm): added validation on form

--- src/components/CreateTrackForm.vue
+++ src/components/CreateTrackForm.vue
@@ -1,21 +1,27 @@
 <template>
-  <v-form @submit.prevent="createTrack">
+  <v-form ref="form" v-model="valid" @submit.prevent="createTrack">
     <v-container>
       <v-text-field
         v-model="title"
         label="Song title"
         placeholder="Colchique dans les prés"
+        :rules="requiredRule"
+        required
       />
       <v-text-field
         v-model="artist"
         label="Artist"
         placeholder="Casandra Lee"
+        :rules="requiredRule"
+        required
       />
       <v-textarea
         v-model="lyrics"
         name="input-7-1"
         label="Lyrics"
         placeholder="🎼Lala la la la lala 🎤"
+        :rules="requiredRule"
+        required
       />
       <v-btn rounded small
              color="primary"
@@ -33,20 +39,26 @@ import { Action } from '@/store/track.store';
 export default {
   data() {
     return {
+      valid: false,
       artist: '',
       genre: 'Unknown',
       lyrics: '' ,
-      title: ''
+      title: '',
+      requiredRule: [value => !!value || '* required' ]
     };
   },
   methods: {
     createTrack() {
-      this.$store.dispatch(Action.CREATE_TRACK, {
-        artist: this.artist,
-        genre: this.genre,
-        lyrics: this.lyrics,
-        title: this.title
-      });
+      this.$refs.form.validate();
+      
+      if (this.valid) {
+        this.$store.dispatch(Action.CREATE_TRACK, {
+          artist: this.artist,
+          genre: this.genre,
+          lyrics: this.lyrics,
+          title: this.title
+        });
+      }
     }
   }
 };
